94cd5f19e76e4198c1f32eb86824bdeb7260c7e9
Warm <1870654579@qq.com>
2020-03-04 16:09:41 +0800

    ([{

--- DS1-introduction/stackPractise01.py
+++ DS1-introduction/stackPractise01.py
@@ -2,7 +2,7 @@
 # 确定列表的哪一端是顶部，然后使用append和pop来实现操作
 
 # 假设列表的尾部是栈的顶部元素， push
-class Stack:
+'''class Stack:
     def __init__(self):
         self.items = []
 
@@ -72,7 +72,48 @@ def parChecker(symbolString):
 
 print(parChecker('(())'))
 print(parChecker('((())'))
+'''
 
+# {[()]} {[()]
+
+# 每一个开始的符号被压入栈，等待匹配结果
+# 当出现结束符号的时候，必须检查栈顶部的开始符号是什么类型，如果两个符号不匹配，则字符串不匹配
+# 整个字符串处理完并且栈为空，则字符串匹配
+
+
+from pythonds.basic.stack import Stack
+
+def parChecker(symbolString):
+    s = Stack()
+    flag = True
+    index = 0
+    
+    while index < len(symbolString) and flag:
+        symbol = symbolString[index]
+
+        if symbol in "([{":     
+            s.push(symbol)
+        else:
+            if s.isEmpty():
+                flag = False
+            else:
+                top = s.pop()  
+                start = '([{'  
+                end = ')]}'   
+
+                if not start.index(top) == end.index(symbol):
+                    flag = False
+        index = index + 1 
+
+
+    if flag and s.isEmpty():
+        return True
+    else:
+        return False
+
+print(parChecker('{[()]}'))
+print(parChecker('{{([][])}()}'))
+print(parChecker('[{()]'))
 
 
 
