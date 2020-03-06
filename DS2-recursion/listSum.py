a19f0886f30131d1601d7a5901aeee6ffdbf8f88
Warm <1870654579@qq.com>
2020-03-06 16:45:11 +0800

    递归栈实现，可视化递归，汉诺塔游戏

--- DS2-recursion/listSum.py
+++ DS2-recursion/listSum.py
@@ -1,12 +1,17 @@
 '''
    [1,3,5,7,9]
 '''
+from pythonds.basic.stack import Stack
+
+
 def listSum(numlist):
     sum = 0
     for i in numlist:
         sum = sum + i
     return sum
-print(listSum([1,3,5,7,9]))
+
+
+print(listSum([1, 3, 5, 7, 9]))
 
 #  不能使用while或者for
 # 使用小学的内容：连加
@@ -27,39 +32,47 @@ print(listSum([1,3,5,7,9]))
 #     if len(numList) == 1:
 #         return numList[0]
 #     else:
-#         return numList[0] + listSum2(numList[1:])  
+#         return numList[0] + listSum2(numList[1:])
 
 # print(listSum2([1,3,5,7,9]))
 
-def toStr(n,base):
+
+def toStr(n, base):
     str1 = '0123456789ABCDEF'
-    # 比如0，1 < 2 
+    # 比如0，1 < 2
     if n < base:
         return str1[n]
     else:
-        return toStr(n//base,base) + str1[n%base]
+        return toStr(n//base, base) + str1[n % base]
 
 
-print(toStr(1453,16))
+print(toStr(1453, 16))
 
 '''
    toStr(10,10) + '0'
    toStr(1,10) + '0'
    '1'
 '''
+# 栈 实现递归
+
+rStack = Stack()
 
 
+def toStr1(n, base):
+    convertString = '0123456789ABCDEF'
+
+    while n > 0:
+        if n < base:
+            rStack.push(convertString[n])
+        else:
+            rStack.push(convertString[n % base])
+
+        n = n // base
+
+    res = ""
+    while not rStack.isEmpty():
+        res = res + str(rStack.pop())
+    return res
 
 
-
-
-
-
-
-
-
-
-
-
-
-
+print(toStr1(1453, 16))
